bcc4db61418f950cdb0253aa70d67746d8e0712d
YuvrajBawa <yuvrajskhera@gmail.com>
2022-07-22 07:44:38 -0700

    Updates

---
+++ precache-manifest.cdaf68f8a09567d49c3e1854b9e97a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1384279f812da31829e7",
+    "url": "/Cryptocurrency-Price-Tracking-App/static/css/main.1ee012e5.chunk.css"
+  },
+  {
+    "revision": "1384279f812da31829e7",
+    "url": "/Cryptocurrency-Price-Tracking-App/static/js/main.1384279f.chunk.js"
+  },
+  {
+    "revision": "f200b66560878bfd1cd8",
+    "url": "/Cryptocurrency-Price-Tracking-App/static/js/1.f200b665.chunk.js"
+  },
+  {
+    "revision": "7ba8022b58326f30ddcd",
+    "url": "/Cryptocurrency-Price-Tracking-App/static/js/runtime~main.7ba8022b.js"
+  },
+  {
+    "revision": "ce1d9d3aafcc70a1850e046c07ec859d",
+    "url": "/Cryptocurrency-Price-Tracking-App/index.html"
+  }
+];
